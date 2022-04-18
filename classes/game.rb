d1508bc1f70be3ca674d092180eddea4cdf20e7e
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-19 00:27:13 +0100

    Override can_be_archived method for the Game class

--- classes/game.rb
+++ classes/game.rb
@@ -1,9 +1,23 @@
 require_relative 'item'
 
 class Game < Item
-  def initialize(publish_date, multiplayer, last_played)
+  def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date)
     @multiplayer = multiplayer
-    @last_played = DateTime.parse(last_played)
+    @last_played_at = DateTime.parse(last_played_at)
+  end
+
+  def last_played_old
+    return true if @last_played_at < DateTime.now.next_year(-2)
+
+    false
+  end
+
+  private :last_played_old
+
+  def can_be_archived?
+    return true if last_played_old && super
+
+    false
   end
 end
