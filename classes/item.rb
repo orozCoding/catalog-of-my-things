34e9fab98c3ab31286e68460f4b1af6d2ca8ba63
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-18 10:43:00 +0100

    Add genre, author and label accessor

--- classes/item.rb
+++ classes/item.rb
@@ -1,11 +1,26 @@
 require 'date'
 
 class Item
-  attr_reader :id, :publish_date, :archived
+  attr_reader :id, :publish_date, :archived, :author, :genre, :label
 
   def initialize(publish_date)
-    @id = Random.rand(1...10000)
+    @id = Random.rand(1...10_000)
     @publish_date = DateTime.parse(publish_date)
     @archived = false
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
   end
 end
