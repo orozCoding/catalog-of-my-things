1b2c21f705339e6f0382931bc65863fcd28863cc
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-19 08:57:37 +0100

    make the canbe_archived method private

--- classes/game.rb
+++ classes/game.rb
@@ -7,13 +7,11 @@ class Game < Item
     @last_played_at = DateTime.parse(last_played_at)
   end
 
-  def last_played_old
-    return true if @last_played_at < DateTime.now.next_year(-2)
-  end
-
-  private :last_played_old
+  private
 
   def can_be_archived?
-    return true if last_played_old && super
+    return true if super && @last_played_at < DateTime.now.next_year(-2)
+
+    false
   end
 end
