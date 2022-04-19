a1cf3a33649dfb10ecd842cf97213a9187f01e24
orozcoding <orozcoding@gmail.com>
2022-04-19 03:04:42 -0400

    Add book class

---
+++ classes/book.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state, :author
+
+  def initialize(publish_date, publisher, cover_state)
+    super(publish_date)
+    @publisher = publisher
+    @cover_state = cover_state.to_s
+  end
+
+  alias parent_can_be_archived? can_be_archived?
+
+  private
+
+  def can_be_archived?
+    return true if parent_can_be_archived? || cover_state == 'bad'
+
+    false
+  end
+
+end
