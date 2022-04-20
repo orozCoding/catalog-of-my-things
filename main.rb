c545a4b978f21a73f3c6d9fd6d90b560c8eb2418
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-20 10:03:59 +0100

    create main.rb file and list of options

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative './app/app'
+
+def main
+  app = App.new
+
+  puts 'Welcome to School Library App!'
+
+
+  loop do
+    app.list_options
+    option = app.select_option
+    app.run_option(option)
+  end
+end
+
+main
