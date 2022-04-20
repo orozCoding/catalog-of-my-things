61966804e42d17f8e63e43a27568110c3d832a0e
uae <aniekaudo@yahoo.com>
2022-04-20 12:40:40 +0100

    :zap: persisting data
    Storing data in Json file and persisting

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -13,5 +13,21 @@ class MusicAlbum < Item
     return true if super && @on_spotify
   end
 
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'music_album' => {
+        'id' => @id,
+        'publish_date' => @publish_date,
+        'on_spotify' => @on_spotify,
+        'author' => @author,
+        'genre' => @genre,
+        'label' => @label,
+        'source' => @source
+      }
+    }.to_json(*args)
+  end
+
+
   private :can_be_archived?
 end
